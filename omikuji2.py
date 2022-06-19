0e8b18095627e5d45a520ecb92959ffd8e15ba7a
yaaashi84 <yaaaashi.84@gmail.com>
2022-06-19 14:21:14 +0900

    リファクタリング

--- omikuji2.py
+++ omikuji2.py
@@ -3,10 +3,13 @@
 """
 import random
 
-omikuji = ["大吉", "吉", "凶", "中吉"]
+omikuji = ["大吉", "吉", "凶", "中吉", "末吉"]
 
-index = random.randint(0, 3)
-result = omikuji[index]
+# index = random.randint(0, len(omikuji) - 1) # おみくじのリストの中の乱数を取得する(これは読みにくいらしい)
+# result = omikuji[index]
+# 一行にしたいときは、randintではなくchoiceを使用する
+
+result = random.choice(omikuji)
 
 print(f"今日の運勢は… {result}")
 
